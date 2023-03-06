a8dfb52f572897689c5cd1b7d49de0b214f5fc18
Krisna Dana <krisnadanaofficial@gmail.com>
2023-03-06 18:13:22 +0800

    add api routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\UserController;
+use App\Http\Controllers\API\BookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
+Route::get('/logout', [UserController::class, 'logout']);
+Route::get('/me', [UserController::class, 'me']);
+
+
+Route::get('/books', [BookController::class, 'index']);
+Route::get('/book/{id}', [UserController::class, 'show']);
+Route::post('/book', [UserController::class, 'store']);
+Route::patch('/book/{id}', [UserController::class, 'update']);
+Route::delete('/book/{id}', [UserController::class, 'destroy']);
+
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
