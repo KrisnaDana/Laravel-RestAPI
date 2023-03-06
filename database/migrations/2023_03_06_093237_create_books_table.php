e7789a6265f38e169baeadbfeb061eb3ecb0c75a
Krisna Dana <krisnadanaofficial@gmail.com>
2023-03-06 17:59:08 +0800

    add resources

--- database/migrations/2023_03_06_093237_create_books_table.php
+++ database/migrations/2023_03_06_093237_create_books_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('books', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->text('descriptions')->nullable();
+            $table->text('description')->nullable();
             $table->foreignId('user_id')->constrained('users');
             $table->timestamps();
         });
